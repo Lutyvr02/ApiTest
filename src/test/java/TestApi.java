8a5a9532358cbb6a18c0a00f5db73b6beeae2e7c
Lutyvr02 <lutyvr02@gmail.com>
2023-10-10 12:37:01 -0400

    Homework commit

--- src/test/java/TestApi.java
+++ src/test/java/TestApi.java
@@ -6,7 +6,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class TestApi {
-   /* @Test
+    @Test
     public void createUpdateReadDeleteProject() {
         JSONObject bodyProject = new JSONObject();
         bodyProject.put("Content", "EynarJSON");
@@ -90,7 +90,7 @@ public class TestApi {
                 .body("Icon", equalTo(2));
 
 
-    }/***************************************************************************************/
+    }
 
     @Test
     public void createUpdateReadDeleteItem() {
